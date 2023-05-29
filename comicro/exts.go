a49244d3cee96c7512f6d1f3954b3b27c019f081
Walter Schulze <awalterschulze@gmail.com>
2023-05-29 16:23:42 +0100

    copy micro to comicro

---
+++ comicro/exts.go
@@ -0,0 +1,80 @@
+package comicro
+
+import (
+	"github.com/awalterschulze/gominikanren/sexpr/ast"
+)
+
+/*
+exts either extends a substitution s with an association between the variable x and the value v ,
+or it produces (ok = false)
+if extending the substitution with the pair `(,x . ,v) would create a cycle.
+
+scheme code:
+
+	(define (ext-s x v s)
+		(cond
+			(
+				(occurs? x v s)
+				#f
+			)
+			(else
+				(cons
+					`(,x . ,v)
+					s
+				)
+			)
+		)
+	)
+*/
+func exts(x *ast.Variable, v *ast.SExpr, s Substitutions) (Substitutions, bool) {
+	if occurs(x, v, s) {
+		return nil, false
+	}
+	m := make(Substitutions, len(s)+1)
+	copy(m, s)
+	m[len(s)] = SubPair{Key: x.Index, Value: v}
+	return m, true
+}
+
+/*
+scheme code:
+
+	(define (occurs? x v s)
+		(let
+			(
+				(v
+					(walk v s)
+				)
+			)
+			(cond
+				(
+					(var? v)
+					(eqv? v x)
+				)
+				(
+					(pair? v)
+					(or
+						(occurs? x (car v) s)
+						(occurs? x (cdr v) s)
+					)
+				)
+				(else
+					#f
+				)
+			)
+		)
+	)
+*/
+func occurs(x *ast.Variable, v *ast.SExpr, s Substitutions) bool {
+	vv := v
+	if v.IsVariable() {
+		vv = walk(v.Atom.Var, s)
+	}
+	if vv.IsVariable() {
+		return vv.Atom.Var.Equal(x)
+	}
+	if vv.IsPair() {
+		return occurs(x, vv.Car(), s) || occurs(x, vv.Cdr(), s)
+	}
+	return false
+}
---
+++ comicro/reify_test.go
@@ -0,0 +1,77 @@
+package comicro
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/awalterschulze/gominikanren/sexpr/ast"
+)
+
+/*
+scheme code:
+
+	(let
+		(
+			(a1 `(,x . (,u ,w ,y ,z ((ice) ,z))))
+			(a2 `(,y . corn))
+			(a3 `(,w . (,v ,u)))
+		)
+		(let
+			(
+				(s `(,a1 ,a2 ,a3))
+			)
+			(
+				(reify x) s
+			)
+		)
+	)
+*/
+func TestReify(t *testing.T) {
+	u := ast.NewVar("u", 0)
+	v := ast.NewVar("v", 1)
+	w := ast.NewVar("w", 2)
+	x := ast.NewVar("x", 3)
+	y := ast.NewVar("y", 4)
+	z := ast.NewVar("z", 5)
+	a1 := ast.NewList(x, u, w, y, z, ast.NewList(ast.NewList(ast.NewSymbol("ice")), z))
+	a2 := ast.Cons(y, ast.NewSymbol("corn"))
+	a3 := ast.NewList(w, v, u)
+	e := ast.NewList(a1, a2, a3)
+	ss := Substitutions{
+		SubPair{indexOf(x), e.Car().Cdr()},
+		SubPair{indexOf(y), e.Cdr().Car().Cdr()},
+		SubPair{indexOf(w), e.Cdr().Cdr().Car().Cdr()},
+	}
+	gote := ReifyIntVarFromState(indexOf(x))(&State{Substitutions: ss})
+	got := gote.String()
+	want := "(_0 (_1 _0) corn _2 ((ice) _2))"
+	if got != want {
+		t.Fatalf("got %s != want %s", got, want)
+	}
+}
+
+func TestNoReify(t *testing.T) {
+	x := ast.NewVariable("x")
+	e1 := EqualO(
+		ast.NewSymbol("olive"),
+		x,
+	)
+	e2 := EqualO(
+		ast.NewSymbol("oil"),
+		x,
+	)
+	g := Disj(e1, e2)
+	states := RunGoal(5, g)
+	ss := make([]*ast.SExpr, len(states))
+	strs := make([]string, len(states))
+	r := ReifyIntVarFromState(indexOf(x))
+	for i, s := range states {
+		ss[i] = r(s)
+		strs[i] = ss[i].String()
+	}
+	got := "(" + strings.Join(strs, " ") + ")"
+	want := "(olive oil)"
+	if got != want {
+		t.Fatalf("got %s != want %s", got, want)
+	}
+}
