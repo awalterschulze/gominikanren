66eca6faa7433033ac28d7a92372fd71034be7ca
Walter Schulze <awalterschulze@gmail.com>
2023-06-10 12:57:32 +0100

    Create SDerivOs for easier debugging

--- example/regex/matcho.go
+++ example/regex/matcho.go
@@ -9,18 +9,25 @@ func MatchO(r, s, res *ast.SExpr) comicro.Goal {
 	if s == nil {
 		return NullO(r, res)
 	}
+	return comicro.CallFresh(func(dr *ast.SExpr) comicro.Goal {
+		return comicro.Conj(
+			SDerivOs(r, s, dr),
+			NullO(dr, res),
+		)
+	})
+}
+
+func SDerivOs(r, s, res *ast.SExpr) comicro.Goal {
+	if s == nil {
+		return comicro.EqualO(res, r)
+	}
 	if !s.IsPair() {
-		return comicro.CallFresh(func(dr *ast.SExpr) comicro.Goal {
-			return comicro.Conj(
-				SDerivO(r, s, dr),
-				NullO(dr, res),
-			)
-		})
+		return SDerivO(r, s, res)
 	}
 	return comicro.CallFresh(func(dr *ast.SExpr) comicro.Goal {
 		return comicro.Conj(
 			SDerivO(r, s.Car(), dr),
-			MatchO(dr, s.Cdr(), res),
+			SDerivOs(dr, s.Cdr(), res),
 		)
 	})
 }
