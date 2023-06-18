ba547e0ac3dd1b2a51ce7ee8429e5119648dd5fd
Walter Schulze <awalterschulze@gmail.com>
2023-06-18 13:49:15 +0100

    remove Fresh to make new Var types easier to implement during the first refactoring

--- comicro/fresh.go
+++ comicro/fresh.go
@@ -11,17 +11,3 @@ func CallFresh(f func(Var) Goal) Goal {
 		f(v)(ctx, s1, ss)
 	}
 }
-
-// calls n new fresh vars, it is up to the user to use them
-// (ie by assigning them to go vars in f)
-// not ideal, but better than 5 nested callfresh calls...
-func Fresh(n int, f func(...Var) Goal) Goal {
-	return func(ctx context.Context, s *State, ss StreamOfStates) {
-		s1 := s
-		vars := make([]Var, n)
-		for i := 0; i < n; i++ {
-			s1, vars[i] = s1.NewVar()
-		}
-		f(vars...)(ctx, s1, ss)
-	}
-}
