12d25f202fe5fa9a5e43781f47cb91b176ec9445
Walter Schulze <awalterschulze@gmail.com>
2023-06-19 16:50:29 +0100

    rename some files and move tests around

--- comicro/goal_test.go
+++ comicro/goal_test.go
@@ -280,3 +280,60 @@ func TestRunGoalConj2OneResults(t *testing.T) {
 		t.Fatalf("got %s != want %s", got, want)
 	}
 }
+
+/*
+scheme code:
+
+	(run-goal 1
+		(call/fresh kiwi
+			(lambda (fruit)
+				(== plum fruit)
+			)
+		)
+	)
+*/
+func TestExistsKiwi(t *testing.T) {
+	s := NewEmptyState().WithVarCreators(ast.CreateVar)
+	ss := RunGoal(context.Background(), 1, s,
+		Exists(func(fruit *ast.SExpr) Goal {
+			return EqualO(
+				ast.NewSymbol("plum"),
+				fruit,
+			)
+		},
+		),
+	)
+	if len(ss) != 1 {
+		t.Fatalf("expected %d, but got %d results", 1, len(ss))
+	}
+	want := "({,v0: plum} . 1)"
+	got := ss[0].String()
+	if got != want {
+		t.Fatalf("got %s != want %s", got, want)
+	}
+}
+
+func TestPineapple(t *testing.T) {
+	pineapple := "pineapple"
+	pointy := &pineapple
+	reify := func(varTyp any, name string) (any, bool) {
+		return &name, true
+	}
+	s := NewEmptyState().WithVarCreators(reify)
+	ss := Run(context.Background(), 1, s,
+		func(fruit *string) Goal {
+			return EqualO(
+				pointy,
+				fruit,
+			)
+		},
+	)
+	if len(ss) != 1 {
+		t.Fatalf("expected %d, but got %d results", 1, len(ss))
+	}
+	got := *(ss[0]).(*string)
+	want := pineapple
+	if got != want {
+		t.Fatalf("got %s != want %s", got, want)
+	}
+}
--- comicro/lookup_test.go
+++ comicro/lookup_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/awalterschulze/gominikanren/sexpr/ast"
 )
 
-func TestWalk(t *testing.T) {
+func TestLookup(t *testing.T) {
 	s := NewEmptyState()
 	var a, v, w, x, y, z *ast.SExpr
 	s, v = NewVarWithName(s, "v", &ast.SExpr{})
@@ -58,63 +58,6 @@ func TestWalk(t *testing.T) {
 			}
 			if want != got {
 				t.Fatalf("got %s want %s", got, want)
-			}
-		})
-	}
-}
-
-func TestReplaceAll(t *testing.T) {
-	s := NewEmptyState()
-	var a, v, w, x, y, z *ast.SExpr
-	s, v = NewVarWithName(s, "v", &ast.SExpr{})
-	s, w = NewVarWithName(s, "w", &ast.SExpr{})
-	s, x = NewVarWithName(s, "x", &ast.SExpr{})
-	s, y = NewVarWithName(s, "y", &ast.SExpr{})
-	s, z = NewVarWithName(s, "z", &ast.SExpr{})
-	xvar, _ := s.GetVar(x)
-	yvar, _ := s.GetVar(y)
-	zvar, _ := s.GetVar(z)
-	wvar, _ := s.GetVar(w)
-	vvar, _ := s.GetVar(v)
-	zaxwyz := s.Copy()
-	zaxwyz, a = NewVarWithName(zaxwyz, "a", &ast.SExpr{})
-	zaxwyz = zaxwyz.AddKeyValue(zvar, a)
-	zaxwyz = zaxwyz.AddKeyValue(xvar, w)
-	zaxwyz = zaxwyz.AddKeyValue(yvar, z)
-	xyvxwx := s.Copy()
-	xyvxwx = xyvxwx.AddKeyValue(xvar, y)
-	xyvxwx = xyvxwx.AddKeyValue(vvar, x)
-	xyvxwx = xyvxwx.AddKeyValue(wvar, x)
-	xbxywxe := s.Copy()
-	xbxywxe = xbxywxe.AddKeyValue(xvar, ast.NewSymbol("b"))
-	xbxywxe = xbxywxe.AddKeyValue(zvar, y)
-	xbxywxe = xbxywxe.AddKeyValue(wvar, ast.NewList(x, ast.NewSymbol("e"), z))
-	xezxyz := s.Copy()
-	xezxyz = xezxyz.AddKeyValue(xvar, ast.NewSymbol("e"))
-	xezxyz = xezxyz.AddKeyValue(zvar, x)
-	xezxyz = xezxyz.AddKeyValue(yvar, z)
-	tests := []func() (Var, *State, string){
-		tuple3(zvar, zaxwyz, "a"),
-		tuple3(yvar, zaxwyz, "a"),
-		tuple3(xvar, zaxwyz, "w"),
-		tuple3(xvar, xyvxwx, "y"),
-		tuple3(vvar, xyvxwx, "y"),
-		tuple3(wvar, xyvxwx, "y"),
-		tuple3(wvar, xbxywxe, "(b e "+y.String()+")"),
-		tuple3(yvar, xezxyz, "e"),
-	}
-	for _, test := range tests {
-		start, state, want := test()
-		startName := state.GetName(start)
-		t.Run("(walk "+startName+" "+state.String()+")", func(t *testing.T) {
-			got := rewrite(start, state)
-			if vgot, ok := state.GetVar(got); ok {
-				got = state.GetName(vgot)
-			} else {
-				got = got.(interface{ String() string }).String()
-			}
-			if want != got {
-				t.Fatalf("got %T:%s want %T:%s", got, got, want, want)
 			}
 		})
 	}
