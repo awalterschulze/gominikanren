4874c3128db02c597ba4623118d773b59f4aaa7d
Walter Schulze <awalterschulze@gmail.com>
2023-06-19 17:11:38 +0100

    remove more pointer constraints to make things ready for slices

--- comicro/goal.go
+++ comicro/goal.go
@@ -14,14 +14,14 @@ func RunGoal(ctx context.Context, n int, s *State, g Goal) []*State {
 }
 
 // Run behaves like the default miniKanren run command
-func Run[A any](ctx context.Context, n int, s *State, g func(*A) Goal) []any {
+func Run[A any](ctx context.Context, n int, s *State, g func(A) Goal) []any {
 	ss := RunStream(ctx, s, g)
 	return Take(ctx, n, ss)
 }
 
 // RunStream behaves like the default miniKanren run command, but returns a stream of answers
-func RunStream[A any](ctx context.Context, s *State, g func(*A) Goal) chan any {
-	var v *A
+func RunStream[A any](ctx context.Context, s *State, g func(A) Goal) chan any {
+	var v A
 	s, v = NewVar(s, v)
 	ss := NewStreamForGoal(ctx, g(v), s)
 	res := make(chan any, 0)
