ac8f5b954453c62708acb5c4f693649547f34312
Walter Schulze <awalterschulze@gmail.com>
2023-06-19 16:50:29 +0100

    rename some files and move tests around

--- comicro/rewrite_test.go
+++ comicro/rewrite_test.go
@@ -27,7 +27,7 @@ scheme code:
 		)
 	)
 */
-func TestReify(t *testing.T) {
+func TestRewrite1(t *testing.T) {
 	s := NewEmptyState().WithVarCreators(ast.CreateVar)
 	var x, u, v, w, y, z *ast.SExpr
 	s, u = NewVar(s, &ast.SExpr{})
@@ -54,7 +54,7 @@ func TestReify(t *testing.T) {
 	}
 }
 
-func TestNoReify(t *testing.T) {
+func TestNoRewrites(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	initial := NewEmptyState().WithVarCreators(ast.CreateVar)
@@ -83,5 +83,62 @@ func TestNoReify(t *testing.T) {
 	want2 := "(oil olive)"
 	if got != want1 && got != want2 {
 		t.Fatalf("got %s != want %s or %s", got, want1, want2)
+	}
+}
+
+func TestRewriteInternal(t *testing.T) {
+	s := NewEmptyState()
+	var a, v, w, x, y, z *ast.SExpr
+	s, v = NewVarWithName(s, "v", &ast.SExpr{})
+	s, w = NewVarWithName(s, "w", &ast.SExpr{})
+	s, x = NewVarWithName(s, "x", &ast.SExpr{})
+	s, y = NewVarWithName(s, "y", &ast.SExpr{})
+	s, z = NewVarWithName(s, "z", &ast.SExpr{})
+	xvar, _ := s.GetVar(x)
+	yvar, _ := s.GetVar(y)
+	zvar, _ := s.GetVar(z)
+	wvar, _ := s.GetVar(w)
+	vvar, _ := s.GetVar(v)
+	zaxwyz := s.Copy()
+	zaxwyz, a = NewVarWithName(zaxwyz, "a", &ast.SExpr{})
+	zaxwyz = zaxwyz.AddKeyValue(zvar, a)
+	zaxwyz = zaxwyz.AddKeyValue(xvar, w)
+	zaxwyz = zaxwyz.AddKeyValue(yvar, z)
+	xyvxwx := s.Copy()
+	xyvxwx = xyvxwx.AddKeyValue(xvar, y)
+	xyvxwx = xyvxwx.AddKeyValue(vvar, x)
+	xyvxwx = xyvxwx.AddKeyValue(wvar, x)
+	xbxywxe := s.Copy()
+	xbxywxe = xbxywxe.AddKeyValue(xvar, ast.NewSymbol("b"))
+	xbxywxe = xbxywxe.AddKeyValue(zvar, y)
+	xbxywxe = xbxywxe.AddKeyValue(wvar, ast.NewList(x, ast.NewSymbol("e"), z))
+	xezxyz := s.Copy()
+	xezxyz = xezxyz.AddKeyValue(xvar, ast.NewSymbol("e"))
+	xezxyz = xezxyz.AddKeyValue(zvar, x)
+	xezxyz = xezxyz.AddKeyValue(yvar, z)
+	tests := []func() (Var, *State, string){
+		tuple3(zvar, zaxwyz, "a"),
+		tuple3(yvar, zaxwyz, "a"),
+		tuple3(xvar, zaxwyz, "w"),
+		tuple3(xvar, xyvxwx, "y"),
+		tuple3(vvar, xyvxwx, "y"),
+		tuple3(wvar, xyvxwx, "y"),
+		tuple3(wvar, xbxywxe, "(b e "+y.String()+")"),
+		tuple3(yvar, xezxyz, "e"),
+	}
+	for _, test := range tests {
+		start, state, want := test()
+		startName := state.GetName(start)
+		t.Run("(walk "+startName+" "+state.String()+")", func(t *testing.T) {
+			got := rewrite(start, state)
+			if vgot, ok := state.GetVar(got); ok {
+				got = state.GetName(vgot)
+			} else {
+				got = got.(interface{ String() string }).String()
+			}
+			if want != got {
+				t.Fatalf("got %T:%s want %T:%s", got, got, want, want)
+			}
+		})
 	}
 }
