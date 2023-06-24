52c8a362924d2d2fede9f5bc73ae18003382e260
Walter Schulze <awalterschulze@gmail.com>
2023-06-24 15:50:48 +0100

    refactoring and math description for concato

--- example/concato/concato.go
+++ example/concato/concato.go
@@ -1,6 +1,8 @@
 package concato
 
 import (
+	"strings"
+
 	"github.com/awalterschulze/gominikanren/comicro"
 	"github.com/awalterschulze/gominikanren/comini"
 )
@@ -12,15 +14,18 @@ type Node struct {
 
 func (n *Node) String() string {
 	if n == nil {
-		return ""
+		return "[]"
 	}
 	res := "<nil>"
 	if n.Val != nil {
 		res = *n.Val
 	}
 	if n.Next != nil {
-		res += n.Next.String()
+		next := n.Next.String()
+		next = strings.TrimSuffix(strings.TrimPrefix(next, "["), "]")
+		res += "," + next
 	}
+	res = "[" + res + "]"
 	return res
 }
 
@@ -28,20 +33,23 @@ func Prepend(val *string, list *Node) *Node {
 	return &Node{val, list}
 }
 
-// Concat is a goal that appends two lists into the third list.
-func ConcatO(xs, ys, out *Node) comicro.Goal {
+// ConcatO is a goal that appends two lists into the third list.
+// ConcatO(xs, ys, zs) ⇔
+// (xs = ∅ ∧ ys = zs) ∨
+// (∃ head, xtail, ztail: xs = Prepend(head, xtail) ∧ zs = Prepend(head, ztail) ∧ ConcatO(xtail, ys, ztail))
+func ConcatO(xs, ys, zs *Node) comicro.Goal {
 	return comicro.Disj(
 		comicro.Conj(
 			comicro.EqualO(xs, nil),
-			comicro.EqualO(ys, out),
+			comicro.EqualO(ys, zs),
 		),
-		comicro.Exists(func(xhead *string) comicro.Goal {
+		comicro.Exists(func(head *string) comicro.Goal {
 			return comicro.Exists(func(xtail *Node) comicro.Goal {
-				return comicro.Exists(func(res *Node) comicro.Goal {
+				return comicro.Exists(func(ztail *Node) comicro.Goal {
 					return comini.Conjs(
-						comicro.EqualO(xs, Prepend(xhead, xtail)),
-						comicro.EqualO(Prepend(xhead, res), out),
-						ConcatO(xtail, ys, res),
+						comicro.EqualO(xs, Prepend(head, xtail)),
+						comicro.EqualO(Prepend(head, ztail), zs),
+						ConcatO(xtail, ys, ztail),
 					)
 				})
 			})
--- example/concato/concato_test.go
+++ example/concato/concato_test.go
@@ -2,11 +2,9 @@ package concato
 
 import (
 	"context"
-	"fmt"
 	"reflect"
 	"sort"
 	"testing"
-	"unsafe"
 
 	"github.com/awalterschulze/gominikanren/comicro"
 	"github.com/awalterschulze/gominikanren/sexpr/ast"
@@ -22,9 +20,17 @@ func CreateVar(varTyp any, name string) (any, bool) {
 	return nil, false
 }
 
+func ref[A any](s A) *A {
+	return &s
+}
+
 type Pair struct {
 	Left  *Node
 	Right *Node
+}
+
+func (p *Pair) String() string {
+	return `{` + p.Left.String() + `, ` + p.Right.String() + `}`
 }
 
 func toStrings(xs []any) []string {
@@ -32,51 +38,33 @@ func toStrings(xs []any) []string {
 		switch a := a.(type) {
 		case *string:
 			return *a
-		case *Node:
-			return a.String()
-		case *Pair:
-			return `"` + a.Left.String() + `","` + a.Right.String() + `"`
 		default:
-			panic(fmt.Sprintf("unknown type %T", a))
+			return a.(Stringer).String()
 		}
 	})
 	sort.Strings(res)
 	return res
 }
 
-func TestSlicePointer(t *testing.T) {
-	s := make([]int, 1)
-	v := reflect.ValueOf(s)
-	p := v.Pointer()
-	fmt.Printf("%v\n", p)
-	h := (*reflect.SliceHeader)(unsafe.Pointer(&s))
-	fmt.Printf("%v\n", h.Data)
-}
-
 func TestPrependO(t *testing.T) {
-	a := "a"
-	b := "b"
-	bs := &Node{&b, nil}
-	as := &Node{&a, nil}
-	abs := &Node{&a, &Node{&b, nil}}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	s := comicro.NewEmptyState().WithVarCreators(CreateVar)
 	gots := toStrings(comicro.Run(ctx, -1, s, func(q *Node) comicro.Goal {
 		return comicro.EqualO(
-			abs,
+			&Node{ref("a"), &Node{ref("b"), nil}},
 			q,
 		)
 	}))
-	wants := toStrings([]any{abs})
+	wants := []string{"[a,b]"}
 	if !reflect.DeepEqual(gots, wants) {
 		t.Fatalf("expected list: got %v, want %v", gots, wants)
 	}
 
 	gots = toStrings(comicro.Run(ctx, -1, s, func(q *Node) comicro.Goal {
 		return PrependO(
-			&a,
-			bs,
+			ref("a"),
+			&Node{ref("b"), nil},
 			q,
 		)
 	}))
@@ -87,33 +75,31 @@ func TestPrependO(t *testing.T) {
 	gots = toStrings(comicro.Run(ctx, -1, s, func(q *string) comicro.Goal {
 		return PrependO(
 			q,
-			bs,
-			abs,
+			&Node{ref("b"), nil},
+			&Node{ref("a"), &Node{ref("b"), nil}},
 		)
 	}))
-	wants = toStrings([]any{as})
+	wants = []string{"a"}
 	if !reflect.DeepEqual(gots, wants) {
 		t.Fatalf("expected consed element: got %v, want %v", gots, wants)
 	}
 
 	gots = toStrings(comicro.Run(ctx, -1, s, func(q *Node) comicro.Goal {
 		return PrependO(
-			&a,
+			ref("a"),
 			q,
-			abs,
+			&Node{ref("a"), &Node{ref("b"), nil}},
 		)
 	}))
-	wants = toStrings([]any{bs})
+	wants = []string{"[b]"}
 	if !reflect.DeepEqual(gots, wants) {
 		t.Fatalf("expected suffix list: got %v, want %v", gots, wants)
 	}
 }
 
 func TestConcatOSingleList(t *testing.T) {
-	a := "a"
-	b := "b"
-	bs := &Node{&b, nil}
-	as := &Node{&a, nil}
+	bs := &Node{ref("b"), nil}
+	as := &Node{ref("a"), nil}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	s := comicro.NewEmptyState().WithVarCreators(CreateVar)
@@ -124,19 +110,14 @@ func TestConcatOSingleList(t *testing.T) {
 			q,
 		)
 	}))
-	wants := []string{"ab"}
+	wants := []string{"[a,b]"}
 	if !reflect.DeepEqual(gots, wants) {
 		t.Fatalf("got %s != want %s", gots, wants)
 	}
 }
 
 func TestConcatOAllCombinations(t *testing.T) {
-	cake := "cake"
-	and := "&"
-	ice := "ice"
-	d := "d"
-	tea := "t"
-	cakeandicedtea := &Node{&cake, &Node{&and, &Node{&ice, &Node{&d, &Node{&tea, nil}}}}}
+	cakeandicedtea := &Node{ref("cake"), &Node{ref("&"), &Node{ref("ice"), &Node{ref("d"), &Node{ref("t"), nil}}}}}
 	if testing.Short() {
 		return
 	}
@@ -161,12 +142,12 @@ func TestConcatOAllCombinations(t *testing.T) {
 		})
 	}))
 	wants := []string{
-		`"","cake&icedt"`,
-		`"cake","&icedt"`,
-		`"cake&","icedt"`,
-		`"cake&ice","dt"`,
-		`"cake&iced","t"`,
-		`"cake&icedt",""`,
+		`{[], [cake,&,ice,d,t]}`,
+		`{[cake,&,ice,d,t], []}`,
+		`{[cake,&,ice,d], [t]}`,
+		`{[cake,&,ice], [d,t]}`,
+		`{[cake,&], [ice,d,t]}`,
+		`{[cake], [&,ice,d,t]}`,
 	}
 	if !reflect.DeepEqual(gots, wants) {
 		t.Fatalf("got %s != want %s", gots, wants)
